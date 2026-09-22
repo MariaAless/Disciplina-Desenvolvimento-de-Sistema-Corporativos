ff67e39304527870623d769cbd587d7c12695b83
MariaAless <alessandraaquino2003@gmail.com>
2026-09-21 23:24:52 -0300

    feat: add serialVersionUID

--- sistema-de-biblioteca/src/main/java/com/alessandra/sistema_de_biblioteca/models/LoanItem.java
+++ sistema-de-biblioteca/src/main/java/com/alessandra/sistema_de_biblioteca/models/LoanItem.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_ITEM_EMPRESTIMO")
 public class LoanItem implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
